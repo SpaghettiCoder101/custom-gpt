8a11183a450f189a4ffd15e22f952c3e70d3ef87
SpaghettiCoder101 <spaghetticoder101@gmail.com>
2023-06-29 12:55:27 +0200

    Added stop button to stop generating an aswer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import ChatMessage from "@/components/ChatMessage";
 import TokenInput from "@/components/TokenInput";
+import StopButton from "@/fragments/StopButton";
 import { Popover } from "@headlessui/react";
 import { useChat } from "ai/react";
 import { useState } from "react";
@@ -10,7 +11,7 @@ export default function Chat() {
     const [messagesToKeep, setMessagesToKeep] = useState<string[]>([]);
     const [tokenToUse, setTokenToUse] = useState<number>(1500);
     const [systemMessage, setSystemMessage] = useState<string>("You are Rondo a heplful AI");
-    const { messages, input, handleInputChange, setMessages, append, setInput } = useChat({
+    const { messages, input, handleInputChange, setMessages, append, setInput, isLoading, stop } = useChat({
         body: {
             systemMessage: systemMessage,
             maxTokens: tokenToUse,
@@ -37,41 +38,44 @@ export default function Chat() {
     }
 
     return (
-        <div className="flex flex-col max-h-screen">
-            <div className="w-full p-4 gap-2 flex border-b-2">
-                <Popover className="bg-blue-500 p-2 w-fit rounded-lg border-solid border-blue-200 border-2">
-                    <Popover.Button className="focus-visible:border-none focus-visible:outline-none text-white">System Message</Popover.Button>
+        <>
+            <div className="flex flex-col max-h-screen">
+                <div className="w-full p-4 gap-2 flex border-b-2">
+                    <Popover className="bg-blue-500 p-2 w-fit rounded-lg border-solid border-blue-200 border-2">
+                        <Popover.Button className="focus-visible:border-none focus-visible:outline-none text-white">System Message</Popover.Button>
 
-                    <Popover.Panel className="absolute z-10 bg-white p-5 rounded-lg shadow-xl">
-                        <textarea 
-                            className="w-full h-32 border border-gray-300 rounded p-2 grow"
-                            value={systemMessage}
-                            onChange={e => setSystemMessage(e.target.value)}
+                        <Popover.Panel className="absolute z-10 bg-white p-5 rounded-lg shadow-xl">
+                            <textarea 
+                                className="w-full h-32 border border-gray-300 rounded p-2 grow"
+                                value={systemMessage}
+                                onChange={e => setSystemMessage(e.target.value)}
+                            />
+
+                        </Popover.Panel>
+                    </Popover>
+                    <TokenInput token={tokenToUse} tokenChange={setTokenToUse} />
+                </div>
+                <div className="mx-auto w-full grow overflow-auto max-w-2xl py-24 flex flex-col stretch mb-8 px-4">
+                    {messages.map(m => (
+                        <ChatMessage key={m.id} message={m} setKeep={keepAddMessage} removeKeep={keepRemoveMessage} />
+                    ))}
+                    <form onSubmit={onSubmit} className="fixed w-full max-w-2xl bottom-8 flex flex-nowrap items-end">
+                        <input
+                            className="border border-gray-300 rounded mr-2 shadow-xl p-2 grow"
+                            value={input}
+                            onChange={handleInputChange}
                         />
-
-                    </Popover.Panel>
-                </Popover>
-                <TokenInput token={tokenToUse} tokenChange={setTokenToUse} />
-            </div>
-            <div className="mx-auto w-full grow overflow-auto max-w-2xl py-24 flex flex-col stretch mb-8 px-4">
-                {messages.map(m => (
-                    <ChatMessage key={m.id} message={m} setKeep={keepAddMessage} removeKeep={keepRemoveMessage} />
-                ))}
-
-                <form onSubmit={onSubmit} className="fixed w-full max-w-2xl bottom-8 flex flex-nowrap items-end">
-                    <input
-                        className="border border-gray-300 rounded mr-2 shadow-xl p-2 grow"
-                        value={input}
-                        onChange={handleInputChange}
-                    />
-                    <button
-                        type="submit"
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                    >
+                        <button
+                            type="submit"
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        >
                     Send
-                    </button>
-                </form>
+                        </button>
+                    </form>
+                </div>
             </div>
-        </div>
+            <StopButton isLoading={isLoading} stop={stop} />
+        </>
+
     );
 }
---
+++ src/fragments/StopButton.tsx
@@ -0,0 +1,29 @@
+import { Transition } from "@headlessui/react";
+
+type Props = {
+  isLoading: boolean;
+  stop: () => void;
+}
+
+export default function StopButton( { isLoading, stop }: Props) {
+    return (
+        <div className="fixed bottom-20 left-1/2">
+            <Transition
+                show={isLoading}
+                enter="transition-all duration-300"
+                enterFrom="opacity-0 translate-y-1/2"
+                enterTo="opacity-100 translate-y-0"
+                leave="transition-all duration-300"
+                leaveFrom="opacity-100 translate-y-0"
+                leaveTo="opacity-0 translate-y-1/2"
+            >
+                <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={stop}
+                >
+                      Stop
+                </button>
+            </Transition>
+        </div>
+    );
+}
